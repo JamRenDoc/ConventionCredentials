c823e4c561d56e8b1091e08cad0643ec17946184
JamRenDoc <j.dockendorff@gmail.com>
2018-06-11 06:20:41 -0500

    work continued on getting front and backend communicating nicely

--- ConventionCredentials/models/database.js
+++ ConventionCredentials/models/database.js
@@ -2,15 +2,17 @@ let pg = require('pg');
 let connectionString = {
     connectionString: "postgres://postgres:edumgolem@localhost:5432/postgres"
   };
+var pool = new pg.Pool();
 
-let client = new pg.Client(connectionString);
-client.connect();
-client.query('SELECT * FROM public."Attendees" AS attendees, public."County" AS county WHERE county."CountyId" = attendees."County" ORDER BY attendees."LastName" ASC;', (err, res) => {
-    if (err) {
-        response = ['Error'];
-        console.log(err.stack);
-    } else {
-        console.log(res.rows);
-    }
+pool.connect(connectionString, (err, client, done) => {
+    var results = [];
+
+    var query = client.query('SELECT * FROM public."Attendees" AS attendees, public."County" AS county WHERE county."CountyId" = attendees."County" ORDER BY attendees."LastName" ASC;');
+
+    query.on('row', (row) => {
+        console.log(row);
+        results.push(row);
+    });
+
+    client.on('end', () => { client.end(); });
 });
-client.on('end', () => { client.end(); });
